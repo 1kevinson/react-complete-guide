df72c63a29af51aa7bbb230cd759296f1bfe630c
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-30 14:57:10 +0200

    [Base features and syntax](<app>): Understanding JSX

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
+    // Javascript Code
     return (
       <div className="App">
-          <h1> Hi, react app</h1>
+        <h1> Hi, react app</h1>
       </div>
     );
+
+    // Javascript above it's get compiled to this code
+    /*    return React.createElement("div",null,React.createElement("h1", { className: "App" }, "React App") );*/
   }
 }
 
