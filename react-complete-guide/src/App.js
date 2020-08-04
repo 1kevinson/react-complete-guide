90e1e571b1809026bf367ecac0b7a9e1e77ffb9f
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-04 11:58:35 +0200

    [Working with list and conditional](<>): Handling dynamically content 'The Javascript way'

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -50,6 +50,31 @@ class App extends Component {
       cursor: "pointer",
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "Maxime")}
+            changed={this.nameChangeHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1> Hi, react app</h1>
@@ -57,26 +82,7 @@ class App extends Component {
         <button style={style} onClick={this.togglePersonHandler}>
           Switch Persons
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Maxime")}
-              changed={this.nameChangeHandler}
-            >
-              My Hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
 
