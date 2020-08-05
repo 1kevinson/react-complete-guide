a9a4823f649299d1ac6007aa2dd23ffa182927ab
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-05 10:01:36 +0200

    [Styling React component](<radium>): Using Radium for media queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import Radium from "radium";
+import Radium, { StyleRoot } from "radium";
 import Person from "./Person/Person.js";
 
 class App extends Component {
@@ -108,14 +108,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1> Hi, react app</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1> Hi, react app</h1>
+          <p className={classes.join(" ")}>This is really working!</p>
+          <button style={style} onClick={this.togglePersonHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
 
     // Javascript above it's get compiled to this code
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,11 +2,18 @@ import React from "react";
 
 // Import CSS in js file to use it
 import "./Person.css";
+import Radium, { StyleRoot } from "radium";
 
 // PRESENTATION / DUMB COMPONENT / STATELESS
 const person = (props) => {
+  const style = {
+    "@media (min-width: 500px)": {
+      width: "40%",
+    },
+  };
+
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old !
       </p>
@@ -16,4 +23,4 @@ const person = (props) => {
   );
 };
 
-export default person;
+export default Radium(person);
