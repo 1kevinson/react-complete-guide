1b7d8581586ae78e844fd603f412d1b445f8b842
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-05 12:20:22 +0200

    [Styling React component](<classes>): Css modules & media queries

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,33 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 
-import "./Person.css";
-
-const StyledDiv = styled.div`
-  width: 50%;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 3rem;
-  text-align: center;
-  border-radius: 7px;
-  margin: 1.5rem auto;
-  font-size: 1.5rem;
-
-  @media (min-width: 500px) : {
-    width: "40%";
-  }
-`;
+// CSS Modules is better approach to work with Javascript
+import classes from "./Person.css";
 
 const person = (props) => {
   return (
-    // <div className="Person" style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old !
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
