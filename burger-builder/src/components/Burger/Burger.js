00b6373c5a86b600e4dbb9110b7dc1a4ca27250c
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-11 12:18:22 +0200

    [Burger Builder](<map, key, object>): Outputting Burger Ingredients

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  // Convert an Object into Array and map into key, values ingredients
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (ingKey) => {
+      return [...Array(props.ingredients[ingKey])].map((dumb, i) => {
+        return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+      });
+    }
+  );
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
