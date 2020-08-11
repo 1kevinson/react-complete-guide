1ed203c2fdacd2b661a25b0639b743991b325346
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-11 11:34:02 +0200

    [Burger Builder](<npm>): Starting the burger component

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -11,7 +11,7 @@ class BurgerIngredient extends Component {
     switch (this.props.type) {
       case "bread-top":
         ingredient = (
-          <div className={classes.BreadBottom}>
+          <div className={classes.BreadTop}>
             <div className={classes.Seeds1}> </div>
             <div className={classes.Seeds2}> </div>
           </div>
