31a2b46837c278a0588cf25aadb66d5185f34bd9
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-13 08:37:42 +0200

    [Burger Builder](<>): Implement the Button Component

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,6 +84,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
+  purchaseContinueHandler = () => {
+    console.log('You continue"');
+  };
+
   render() {
     // Copy the ingredient Object
     const disableInfo = {
@@ -101,7 +105,11 @@ class BurgerBuilder extends Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
 
         <Burger ingredients={this.state.ingredients} />
