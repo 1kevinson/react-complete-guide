9f89ead89d760d6aed2a07ab1fb9c5371ecb43f1
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-13 11:03:49 +0200

    [Burger Builder](<>):  Creating a responsive SideDrawer

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import Aux from "../../hoc/Aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
 import classes from "./Layout.css";
 
 // Functional Component to return the layout
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <div> SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
